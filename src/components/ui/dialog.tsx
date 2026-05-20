ce93345c1cf90fa909b3f86ced4f5ea6ab006a66
Gabrielll04 <gabrielggcarneiro04@gmail.com>
2026-05-20 20:46:50 -0300

    fix: remove desalinhamento dos modais ao abrir

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -34,7 +34,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border bg-card p-6 text-card-foreground shadow-lg data-[state=open]:animate-slide-up",
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border bg-card p-6 text-card-foreground shadow-lg data-[state=open]:animate-dialog-in",
         className
       )}
       {...props}
